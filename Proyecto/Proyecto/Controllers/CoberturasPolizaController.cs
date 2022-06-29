678943458d099542e3a82966cbb750bedee6c977
Edgar <alonsomiranda96@gmail.com>
2022-06-28 20:20:56 -0600

    Guardar

---
+++ Proyecto/Proyecto/Controllers/CoberturasPolizaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto.Models;
+
+namespace Proyecto.Controllers
+{
+    public class CoberturasPolizaController : Controller
+    {
+        //Instancia del modelo de la base de datos
+        ProyectoSegurosEntities modeloBD = new ProyectoSegurosEntities();
+
+        // GET: CoberturasPoliza
+        public ActionResult CoberturasPolizaLista()
+        {
+           
+            return View();
+        }
+    }
+}
