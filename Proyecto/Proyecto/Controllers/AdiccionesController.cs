27101f59379d2202b81aa764bcaded86880d0daa
Diego <diegobarbozacoto@gmail.com>
2022-07-01 23:10:36 -0600

    adicciones moficando

--- Proyecto/Proyecto/Controllers/AdiccionesController.cs
+++ Proyecto/Proyecto/Controllers/AdiccionesController.cs
@@ -51,6 +51,42 @@ namespace Proyecto.Controllers
             return View();
         }
 
+        public ActionResult AdiccionesModificar(int id_Adiccion)
+        {
+            sp_Retorna_AdiccionesID_Result modeloVista = new sp_Retorna_AdiccionesID_Result();
+            modeloVista = modeloBD.sp_Retorna_AdiccionesID(id_Adiccion).FirstOrDefault();
+            return View(modeloVista);
 
+        }
+
+        [HttpPost]
+        public ActionResult AdiccionesModificar(sp_Retorna_AdiccionesID_Result modeloVista)
+        {
+            int cantRegistrosAfectados = 0;
+            string resultado = "";
+
+            try
+            {
+                cantRegistrosAfectados = modeloBD.sp_Modificar_Adicciones(modeloVista.Id_Adiccion, modeloVista.Nombre, modeloVista.Codigo);
+            }
+            catch (Exception error)
+            {
+                resultado = "Ocurrió un error: " + error.Message;
+            }
+            finally
+            {
+                if (cantRegistrosAfectados > 0)
+                {
+                    resultado = "Registro modificado";
+                }
+                else
+                {
+                    resultado += ".No se pudo modificar ";
+                }
+            }
+
+            Response.Write("<script language=javascript>alert('" + resultado + "');</script>");
+            return View(modeloVista);
+        }
     }
 }
