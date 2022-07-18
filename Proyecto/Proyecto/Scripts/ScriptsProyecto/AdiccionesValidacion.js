79baf875d2b28d5d683ab86ab30c3947d793e373
Diego <diegobarbozacoto@gmail.com>
2022-07-17 21:56:37 -0600

    adicciones validacion y redirectToAction en controler

---
+++ Proyecto/Proyecto/Scripts/ScriptsProyecto/AdiccionesValidacion.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+    validarAdicciones();
+});
+
+function validarAdicciones() {
+
+    //selector haciendo referencia al form de validación, aplica para insertar y eliminar
+    $("#frmAdicciones").validate({
+        rules: {
+            Nombre: {
+                required: true
+            },
+            Codigo: {
+                required: true,
+                number:true
+            }
+        }
+    });
+}
