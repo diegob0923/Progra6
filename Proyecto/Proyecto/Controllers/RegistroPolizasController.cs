5e5a68bc7e36114cdd6c4ce3fcca7b21c66d2f54
Diego <diegobarbozacoto@gmail.com>
2022-07-20 22:54:02 -0600

    mostrando lista registro polizas

---
+++ Proyecto/Proyecto/Controllers/RegistroPolizasController.cs
@@ -0,0 +1,20 @@
+﻿using Proyecto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto.Controllers
+{
+    public class RegistroPolizasController : Controller
+    {
+        ProyectoSegurosEntities modeloBD = new ProyectoSegurosEntities();
+        public ActionResult RegistroPolizasLista()
+        {
+            List<sp_Retorna_Poliza_Cliente_Result> modeloVista = new List<sp_Retorna_Poliza_Cliente_Result>();
+            modeloVista = modeloBD.sp_Retorna_Poliza_Cliente(null, null).ToList();
+            return View(modeloVista);
+        }
+    }
+}
