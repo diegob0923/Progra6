2d5fdcc29f5f95cd8e089e5c81a117bf34094117
Edgar <alonsomiranda96@gmail.com>
2022-07-31 10:47:22 -0600

    Menús de cliente y colaborador
    Arreglos en el inicio de sesión algunos cambios en los layout, pruebas

--- Proyecto/Proyecto/Controllers/InicioSesionController.cs
+++ Proyecto/Proyecto/Controllers/InicioSesionController.cs
@@ -20,58 +20,27 @@ namespace Proyecto.Controllers
             return View();
         }
 
-        
+        #region diferenciar usuarios 
         [HttpPost]
         public ActionResult InicioSesion(int Usuario, string Contrasena)
         {
-            //Arreglar 
-
-            //RetornarUsuarioContrasena_Result
-            //    Resultado = this.modeloBD.RetornarUsuarioContrasena(Usuario, Contrasena).FirstOrDefault();
-            //string Mensaje = "";
-
-            ///// Verificar si el objeto es nulo, si lo es,
-            ///// entonces el usuario y contraseña son incorrectos
-            //if (Resultado == null)
-            //{
-            //   Mensaje = "Usuario o contraseña incorrectos";
-            //    Response.Write("<script language=javascript>alert('" + Mensaje + "');</script>");
-
-            //}
-            //else
-            //{
-
-            //    if (DatosDoctor == null)
-            //    {
-            //        this.Session.Add("ultimoIngreso", "Esta es su primera vez en el sistema");
-            //    }
-            //    else
-            //    {
-            //        this.Session.Add("ultimoIngreso", DatosDoctor.Fecha_Ultimo_Ingreso);
-            //        this.ModeloBD.InsertarRegistroIngreso(Resultado.correoElectronico);
-            //    }
-            //    // Variables de sesión
-            //    this.ModeloBD.InsertarRegistroIngreso(Resultado.correoElectronico);
-            //    this.Session.Add("nombreUsuario", Resultado.nombreCompleto);
-            //    this.Session.Add("idUsuario", Resultado.id_Usuario);
-            //    this.Session.Add("usuarioLogeado", true);
-            //    this.Session.Add("tipoUsuario", Resultado.tipoUsuario);
-            //    this.Response.Redirect("~/Formularios/FrmPaginaPrincipal.aspx");
-            //}
             
             try
             {
                 using (Models.ProyectoSegurosEntities modeloBD = new ProyectoSegurosEntities())
                 {
-                    //string tipo = "";
+                    
                     string tipoCliente = "Cliente";
                     string tipoColaborador = "Colaborador";
+
                     var usuario1 = (from login in modeloBD.Usuarios_Sistema
                                    where login.Usuario == Usuario && login.Contrasena == Contrasena.Trim() && login.TipoUsuario == tipoCliente
                                    select login).FirstOrDefault();
+
                     var usuario2 = (from login in modeloBD.Usuarios_Sistema
                                     where login.Usuario == Usuario && login.Contrasena == Contrasena.Trim() && login.TipoUsuario == tipoColaborador
                                     select login).FirstOrDefault();
+
                     var TipoCliente = (from T in modeloBD.Usuarios_Sistema where T.TipoUsuario == tipoCliente select T).FirstOrDefault();
                     var TipoColaborador = (from T in modeloBD.Usuarios_Sistema where T.TipoUsuario == tipoColaborador select T).FirstOrDefault();
 
@@ -85,14 +54,14 @@ namespace Proyecto.Controllers
 
 
                         Session["Usuario"] = usuario1;
-                        return RedirectToAction("ClientesLista", "Clientes");
+                        return RedirectToAction("MenuCliente", "MenuCliente");
                     }
                     
                     else if (usuario2 == TipoColaborador)
                     {
                         
                         Session["Usuario"] = usuario2;
-                        return RedirectToAction("AdiccionesClienteReporte", "AdiccionesClienteReporte");
+                        return RedirectToAction("MenuColaborador", "MenuColaborador");
                     }
                     //Session["Usuario"] = usuario;
                 }
@@ -108,5 +77,6 @@ namespace Proyecto.Controllers
             }
 
         }
+        #endregion
     }
 }
